9fb5956c9355bbb2455d03af0fcfa7ae33fb2512
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2021-02-18 18:21:57 +0100

    Run conversion in parallel (use # of CPU's)

--- src/Cube/CubeFaceConverter.cs
+++ src/Cube/CubeFaceConverter.cs
@@ -1,16 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.PixelFormats;
 
+// #define ENABLE_BICUBIC_INTERPOLATION
+
 namespace Cube
 {
     public static class CubeFaceConverter
     {
+        private const double Pi = Math.PI;
+        private const double HalfPi = Pi / 2.0d;
+
         private static readonly JpegEncoder OutputEncoder = new JpegEncoder
         {
             Quality = 85,
@@ -29,31 +36,39 @@ namespace Cube
                 .Select(_ => new Image<Rgb24>(cubeSizeInPixels, cubeSizeInPixels, Color.White))
                 .ToArray();
 
-            await ConvertBack(inputImage, outputFaceImages, cubeSizeInPixels);
+            ConvertBack(inputImage, outputFaceImages, cubeSizeInPixels);
 
-            for (var i = 0; i < outputFaceImages.Length; i++)
+            var saveTasks = outputFaceImages.Select(async (outputImage, index) =>
             {
-                await outputFaceImages[i].SaveAsync($"{outputImagePath}_{i}.jpg", OutputEncoder, cancellationToken);
-                outputFaceImages[i].Dispose();
-            }
+                await outputImage.SaveAsync($"{outputImagePath}_{index}.jpg", OutputEncoder, cancellationToken);
+                outputImage.Dispose();
+            });
+
+            await Task.WhenAll(saveTasks);
         }
 
-        private static Task ConvertBack(Image<Rgb24> inputImage, Image<Rgb24>[] outputImages, int edge)
+        private static void ConvertBack(Image<Rgb24> inputImage, Image<Rgb24>[] outputImages, int edge)
         {
-            for (var face = 0; face < 6; face++)
+            var inputImageWidth = inputImage.Width;
+            var inputImageHeight = inputImage.Height;
+
+            var blocks = GenerateProcessingBlocks(6 * edge, Environment.ProcessorCount);
+
+            Parallel.ForEach(blocks, ((int Start, int End) range) =>
             {
-                for (var i = 0; i < edge; i++)
+                for (var k = range.Start; k < range.End; k++)
                 {
+                    var face = k / edge;
+                    var i = k % edge;
+
                     for (var j = 0; j < edge; j++)
                     {
                         var xyz = OutputImageToVector(i, j, face, edge);
-                        var color = InterpolateVectorToColor(xyz, inputImage);
+                        var color = InterpolateVectorToColor(xyz, inputImage, inputImageWidth, inputImageHeight);
                         outputImages[face][i, j] = color;
                     }
                 }
-            }
-
-            return Task.CompletedTask;
+            });
         }
 
         private static Vector3 OutputImageToVector(int i, int j, int face, int edge)
@@ -73,46 +88,70 @@ namespace Cube
             };
         }
 
-        private static Rgb24 InterpolateVectorToColor(Vector3 xyz, Image<Rgb24> image)
+        private static Rgb24 InterpolateVectorToColor(Vector3 xyz, Image<Rgb24> image, int imageWidth, int imageHeight)
         {
-            var _sw = image.Width;
-            var _sh = image.Height;
-
             var theta = Math.Atan2(xyz.Y, xyz.X); // # range -pi to pi
             var r = Math.Sqrt(xyz.X * xyz.X + xyz.Y * xyz.Y);
             var phi = Math.Atan2(xyz.Z, r); // # range -pi/2 to pi/2
 
             // source img coords
-            var uf = (theta + Math.PI) / Math.PI * _sh;
-            var vf = (Math.PI / 2 - phi) / Math.PI * _sh; // implicit assumption: _sh == _sw / 2
+            var uf = (theta + Pi) / Pi * imageHeight;
+            var vf = (HalfPi - phi) / Pi * imageHeight; // implicit assumption: _sh == _sw / 2
 
             // Use bilinear interpolation between the four surrounding pixels
-            var ui = SafeIndex((int) uf, _sw);  //# coords of pixel to bottom left
-            var vi = SafeIndex((int) vf, _sh);
-            var u2 = SafeIndex(ui + 1, _sw);    //# coords of pixel to top right
-            var v2 = SafeIndex(vi + 1, _sh);
-            var mu = uf - ui; //# fraction of way across pixel
-            var nu = vf - vi;
-            mu = nu = 0; // TODO: What ? :)
+            var ui = SafeIndex((int) uf, imageWidth);  //# coords of pixel to bottom left
+            var vi = SafeIndex((int) vf, imageHeight);
+
+#if ENABLE_BICUBIC_INTERPOLATION
+            var u2 = SafeIndex(ui + 1, imageWidth);    //# coords of pixel to top right
+            var v2 = SafeIndex(vi + 1, imageHeight);
+            var mu = (byte)((uf - ui) * 255); //# fraction of way across pixel
+            var nu = (byte)((vf - vi) * 255);
 
             // Pixel values of four nearest corners
-            var A = image[ui, vi];
-            var B = image[u2, vi];
-            var C = image[ui, v2];
-            var D = image[u2, v2];
+            var a = image[ui, vi];
+            var b = image[u2, vi];
+            var c = image[ui, v2];
+            var d = image[u2, v2];
 
-            return Mix(Mix(A, B, mu), Mix(C, D, mu), nu);
+            return Mix(Mix(a, b, mu), Mix(c, d, mu), nu);
+#else
+            return image[ui, vi];
+#endif
         }
 
-        private static int SafeIndex(int n, int size)
-            => Math.Min(Math.Max(n, 0), size - 1);
+        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        private static int SafeIndex(int n, int size) => Math.Min(Math.Max(n, 0), size - 1);
 
-        private static Rgb24 Mix(Rgb24 one, Rgb24 other, double c)
+#if ENABLE_BICUBIC_INTERPOLATION
+        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        private static Rgb24 Mix(Rgb24 one, Rgb24 other, byte c)
         {
             var red = (byte)(one.R + (other.R - one.R) * c);
             var green = (byte)(one.G + (other.G - one.G) * c);
             var blue = (byte)(one.B + (other.B - one.B) * c);
             return new Rgb24(red, green, blue);
+        }
+#endif
+
+        private static IEnumerable<(int Start, int End)> GenerateProcessingBlocks(int range, int blockCount)
+        {
+            var blockSize = range / Math.Max(blockCount, 1);
+            var start = 0;
+
+            while (true)
+            {
+                var end = start + blockSize;
+                if (end + blockSize > range)
+                    end = range;
+
+                yield return (start, end);
+
+                if (end >= range)
+                    break;
+
+                start = end;
+            }
         }
     }
 }
